54155bbdc8b0e25cce9a4e145456fcf8466dbe17
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-07-12 17:16:43 -0300

    feat: add cost two

--- src/app/modules/data-access/characters/lib/mock/characters.ts
+++ src/app/modules/data-access/characters/lib/mock/characters.ts
@@ -1,4 +1,4 @@
-import { Unity } from "../../models/unity";
+import { Unity } from '../../models/unity';
 
 export const charactersByCost: Unity = {
   1: [
@@ -68,4 +68,74 @@ export const charactersByCost: Unity = {
       photo: '/images/cost-one/yasuo.png',
     },
   ],
+  2: [
+    {
+      name: 'Aatrox',
+      traits: ['Ghostly', 'Inkshadow', 'Bruiser'],
+      photo: '/images/cost-two/aatrox.png',
+    },
+    {
+      name: 'Gnar',
+      traits: ['Dryad', 'Warden'],
+      photo: '/images/cost-two/gnar.png',
+    },
+    {
+      name: 'Janna',
+      traits: ['Dragonlord', 'Invoker'],
+      photo: '/images/cost-two/janna.png',
+    },
+    {
+      name: 'Kindred',
+      traits: ['Dryad', 'Fated', 'Reaper'],
+      photo: '/images/cost-two/kindred.png',
+    },
+    {
+      name: 'Lux',
+      traits: ['Porcelain', 'Arcanist'],
+      photo: '/images/cost-two/lux.png',
+    },
+    {
+      name: 'Neeko',
+      traits: ['Heavenly', 'Mythical', 'Arcanist'],
+      photo: '/images/cost-two/neeko.png',
+    },
+    {
+      name: 'Qiyana',
+      traits: ['Heavenly', 'Duelist'],
+      photo: '/images/cost-two/qiyana.png',
+    },
+    {
+      name: 'Riven',
+      traits: ['Storyweaver', 'Altruist', 'Bruiser'],
+      photo: '/images/cost-two/riven.png',
+    },
+    {
+      name: 'Senna',
+      traits: ['Inkshadow', 'Sniper'],
+      photo: '/images/cost-two/senna.png',
+    },
+    {
+      name: 'Shen',
+      traits: ['Ghostly', 'Behemoth'],
+      photo: '/images/cost-two/shen.png',
+    },
+    {
+      name: 'Teemo',
+      traits: ['Fortune', 'Trickshot'],
+      photo: '/images/cost-two/teemo.png',
+    },
+    {
+      name: 'Yorick',
+      traits: ['Umbral', 'Behemoth'],
+      photo: '/images/cost-two/yorick.png',
+    },
+    {
+      name: 'Zyra',
+      traits: ['StoryWeaver', 'Sage'],
+      photo: '/images/cost-two/zyra.png',
+    },
+  ],
+  3: [],
+  4: [],
+  5: [],
 };

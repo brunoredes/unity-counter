3e4d73d7cb8bdacfc8a1275e370275f202a8fcd8
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-07-12 09:13:59 -0300

    feat: add footer

---
+++ src/app/modules/shell/footer/footer.component.spec.ts
@@ -0,0 +1,31 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FooterComponent } from './footer.component';
+
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FooterComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(FooterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should show the current year', () => {
+    expect(component.year).toBe(2024);
+  });
+
+  it('Should show copyright on small tag', () => {
+    const small = fixture.nativeElement.querySelector('small');
+    expect(small).toBeTruthy();
+  });
+});

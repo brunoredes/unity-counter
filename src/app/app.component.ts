cfdcbfd6305ba964faec3cf89d3e5735efe39a18
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-07-12 15:35:36 -0300

    feat(app): integrate header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './modules/shell/footer/footer.component';
+import { HeaderComponent } from './modules/shell/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ngx-unity-counter';
 }

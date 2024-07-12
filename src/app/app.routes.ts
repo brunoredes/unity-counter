e9b4765a3ca776769a97bd85f4ca7d14f2a68027
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-07-12 09:15:02 -0300

    feat(counter): add component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', loadChildren: () => import('./modules/feature/counter/counter.routes').then(r => r.COUNTER_ROUTES) },
+];
---
+++ src/app/modules/feature/counter/counter.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
+
+export const COUNTER_ROUTES: Routes = [
+  { path: '', component: CounterComponent },
+];

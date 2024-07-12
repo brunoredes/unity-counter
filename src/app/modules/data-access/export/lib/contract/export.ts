c4622941adae781b07293c59d5c308e1d160eb24
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-07-12 15:43:59 -0300

    feat(export): create service

---
+++ src/app/modules/data-access/export/lib/contract/export.ts
@@ -0,0 +1,3 @@
+export interface Export {
+  export(): void;
+}
---
+++ src/app/modules/data-access/export/lib/xlsx/xlsx.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Export } from '../contract/export';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class XlsxService implements Export{
+
+  constructor() { }
+
+  public export() {
+    
+  }
+}

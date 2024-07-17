3ff60372b034fcb0f729fb446d557f9dd853ce18
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-07-16 22:17:48 -0300

    feat(character): add new one cost

---
+++ src/app/modules/data-access/characters/lib/mock/traits.ts
@@ -0,0 +1,32 @@
+export enum Origin {
+    ARCANA = 'Arcana',
+    DRAGON = 'Dragon',
+    ELDRITCH = 'Eldritch',
+    FROST = 'Frost',
+    PORTAL = 'Portal',
+    RAVENOUS = 'Ravenous',
+    WITCHCRAFT = 'Witchcraft',
+    CHRONO = 'Chrono',
+    DRUID = 'Druid',
+    FAERIE = 'Faerie',
+    HONEYMANCY = 'Honeymancy',
+    PYRO = 'Pyro',
+    SUGARCRAFT = 'Sugarcraft'
+}
+
+export enum Class {
+    ASCENDANT = 'Ascendant',
+    BATQUEEN = 'Bat Queen',
+    BLASTER = 'Blaster',
+    INCANTOR = 'Incantor',
+    MULTISTRICKER = 'Multistriker',
+    SCHOLAR = 'Scholar',
+    VANGUARD = 'Vanguard',
+    BASTION = 'Bastion',
+    BESTFRIENDS = 'Best Friend',
+    HUNTER = 'Hunter',
+    MAGE = 'Mage',
+    PRESERVER = 'Preserver',
+    SHAPESHIFTER = 'Shapeshifter',
+    WARRIOR = 'Warrior'
+}

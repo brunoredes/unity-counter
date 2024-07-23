73e7f1564a264229b0cdd5c99b94aa35ec513da2
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-07-23 11:26:03 -0300

    feat: add angular pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,20 @@
 import {
   ApplicationConfig,
-  provideExperimentalZonelessChangeDetection
+  provideExperimentalZonelessChangeDetection, isDevMode
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideCloudflareLoader } from '@angular/common';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
-    provideCloudflareLoader('https://imagedelivery.net')
+    provideCloudflareLoader('https://imagedelivery.net'), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ],
 };
